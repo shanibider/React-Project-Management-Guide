1357641bbefb7e779056ca0c3e901c36cc85efe7
Shani Bider <shanibider@gmail.com>
2024-06-24 16:34:30 +0300

    first commit

---
+++ 02-styling-sidebar/src/App.jsx
@@ -0,0 +1,20 @@
+import ProjectsSidebar from './components/ProjectsSidebar.jsx';
+
+/*
+Advanced Concepts -
+Working with Components, State, Styling and Portals;
+* Build, style and reuse components
+* Manage state 
+* Access DOM elements with refs
+* Manage JSX rendering positions with portals
+*/
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+      <ProjectsSidebar />
+    </main>
+  );
+}
+
+export default App;
